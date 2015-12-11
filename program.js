169de66b82c577c97d5fd556b5767f26b5ba315a
John Sonneville <SonnevilleJ@gmail.com>
2015-12-11 00:47:37 -0600

    Exercise 8 complete

--- program.js
+++ program.js
@@ -1,8 +1,9 @@
-var http = require('http');
+const http = require('http'),
+    bl = require('bl');
 
 http.get(process.argv[2], function (response) {
-    response.setEncoding('utf8');
-    response.on('data', function(data){
-        console.log(data);
-    });
+    response.pipe(bl(function(err, data) {
+        console.log(data.toString().length);
+        console.log(data.toString());
+    }));
 });
