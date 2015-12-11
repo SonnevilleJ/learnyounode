f295288d063cc25150ca14234cc39a5d6eac3606
John Sonneville <SonnevilleJ@gmail.com>
2015-12-10 22:05:33 -0600

    Exercise 4 complete

--- program.js
+++ program.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
-var contents = fs.readFileSync(process.argv[2]).toString();
-var count = contents.split('\n').length - 1;
-
-console.log(count);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+    if (err) throw err;
+    var count = data.toString().split('\n').length - 1;
+    console.log(count);
+});
